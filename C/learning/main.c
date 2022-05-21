239d94b0e84145214eaf59e41f1d80dfbd44465d
Professional-Kate <professionalkateg@gmail.com>
2022-05-21 13:36:26 +0100

    Hello, World in C

---
+++ C/learning/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // allows us to use printf
+
+int main() 
+{
+  printf("Hello, World!\n"); // print a line to the standard output
+  return 0; // 0 == everything okay! Exit success
+}
