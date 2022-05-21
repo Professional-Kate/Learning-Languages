fda88291fb20ad28f1c18e813ac4182ba850e5bb
Professional-Kate <professionalkateg@gmail.com>
2022-05-21 13:48:45 +0100

    String interpolation, is that how you spell that? Not even sure if that's the right term, the printf() has % as a placeholder charater. So when it encouters one it reads the next charater (d = int, s = string) then takes the next argument and prints it as that type

--- C/learning/main.c
+++ C/learning/main.c
@@ -2,6 +2,9 @@
 
 int main() 
 {
-  printf("Hello, World!\n"); // print a line to the standard output
+  char myName[] = "Katie"; // char array of my name : string. Each charater has a place in memory, the whole string isn't stored at one memory address 
+  int myAge = 22;
+  printf("Hello there, my name is %s\n", myName); // %s is replaced with the string. %s is the identifier for a string being interpolated into the string
+  printf("%s was %d years old \n", myName, myAge); // %d is the same as above but this time with an intiger
   return 0; // 0 == everything okay! Exit success
 }
