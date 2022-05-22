d469b6c069da5262ed77c0637de20e5813736480
Professional-Kate <professionalkateg@gmail.com>
2022-05-22 21:09:57 +0100

    started working a project to get user input and then print that in the stdout. Learning more about pointers

--- C/LearningC/main.c
+++ C/LearningC/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+// returns a char pointer for our full char[]
+char* readUserInput()
+{
+  // will update to actually read from a users input in the stdin. Right now hardcoded so I understand the theory
+  char* string = malloc(4); // allocate 4 places in memory to our string
+  string[0] = 'A';
+  string[1] = 'B';
+  string[2] = 'C';
+  string[3] = '\0'; // null terminator so C knows where the char[] ends
+
+  return string;
+}
+
 int main()
 {
-    //int usersAge;
-    //printf("What is your favourite number? ");
-    //scanf("%d", &usersAge); get user input BUT nothing after a space charater
-    //printf("%d is a stupid number.\n");
+  char* returnValue = readUserInput(); // capture that pointer to be used later
+  printf("%s", returnValue);
+  free(returnValue); // free the memory we use for the varible so we don't memory leak
 
-    const int maxInput = 20; // don't want the user to overload the buffer
-    char usersName[maxInput]; // char array of length maxInput
-    printf("What is your favourite word? ");
-    fgets(usersName, maxInput, stdin);
-    // fgets(variable to save to, max input size, where to get the input from);
-    printf("I can't believe your favourite word is: %s", usersName);
-    // the issue with fgets is that it takes the enter charater as an input. Which means after the users input it will include the a line break
-
-    return 0;
+  return 0;
 }
