8106f582b781fa23e9162a08cf830ac7c7593ac9
Professional-Kate <professionalkateg@gmail.com>
2022-06-30 13:42:57 +0100

    finished the script, gonna work on it more later with some feedback I get

--- C/LearningC/main.c
+++ C/LearningC/main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// returns a char pointer for our full char[]
-char* readUserInput()
-{
-  // will update to actually read from a users input in the stdin. Right now hardcoded so I understand the theory
-  char* string = malloc(4); // allocate 4 places in memory to our string
-  string[0] = 'A';
-  string[1] = 'B';
-  string[2] = 'C';
-  string[3] = '\0'; // null terminator so C knows where the char[] ends
-
-  return string;
-}
+char* readUserInput(int maxInput); // prototype for our function below
 
 int main()
 {
-  char* returnValue = readUserInput(); // capture that pointer to be used later
-  printf("%s", returnValue);
+  char* returnValue = readUserInput(255); // capture that pointer to be used later
+  printf("You Wrote: %s", returnValue);
   free(returnValue); // free the memory we use for the variable so we don't memory leak
 
   return 0;
+}
+
+// returns a char pointer for our full string
+char* readUserInput(int maxInput)
+{
+  char* string = malloc(maxInput); // allocate 255 places in memory for user input
+  printf("Write anything you want: ");
+  fgets(string, maxInput, stdin); // read something from the stdin and write that into the first argument
+  int currentSize = 0;
+
+  // looping over our string to find the '\0' character
+  while (1) 
+  {
+    if (string[currentSize] == '\0') 
+    {
+      break;
+    }
+    currentSize++;
+  }
+  
+  realloc(string, currentSize); // reallocate our variable to only use the amount of memory we need
+
+  return string;
 }
