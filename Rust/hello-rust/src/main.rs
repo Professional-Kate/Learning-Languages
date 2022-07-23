d6d6c3e47097ab5e634e28ccb8e4feb9ac0cfd9c
Professional-Kate <professionalkateg@gmail.com>
2022-07-23 16:09:45 +0100

    following through the starter project on the Rust website

--- Rust/hello-rust/src/main.rs
+++ Rust/hello-rust/src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello, Tea!"); // define a string and assign that to "message"
+    let width = message.chars().count(); // break up the sting into a char[] then count every element
+
+    let mut writer = BufWriter::new(stdout.lock()); // define a mutable variable so it can be borrowed later 
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
